b0c932a613926b523e25009992f150cb5b480327
kylecodes <kylemuldoon15@gmail.com>
2019-08-26 01:04:53 -0700

    added unit cube to start displaying soon

--- 3D_Graphics_Engine/3D_Graphics_Engine/3D_Graphics_Engine.cpp
+++ 3D_Graphics_Engine/3D_Graphics_Engine/3D_Graphics_Engine.cpp
@@ -2,16 +2,65 @@
 #include "olcConsoleGameEngine.h"
 using namespace std;
 
+struct vec3d
+{
+	float x, y, z;
+};
+
+struct triangle
+{
+	vec3d p[3];
+};
+
+struct mesh
+{
+	vector<triangle> tris;
+};
+
 class engine3D : public olcConsoleGameEngine
 {
+private: 
+	mesh meshCube;
+
 public:
 	engine3D()
 	{
+		m_sAppName = L"3d Demo";
 	}
 
 public:
 	bool OnUserCreate() override
 	{
+		// Creating a unit cube for testing the display functionality
+
+		meshCube.tris = {
+
+			// SOUTH
+			{0.0f, 0.0f, 0.0f,	0.0f, 1.0f, 0.0f,	1.0f, 1.0f, 0.0f},
+			{0.0f, 0.0f, 0.0f,	1.0f, 1.0f, 0.0f,	1.0f, 0.0f, 0.0f},
+
+			// EAST
+			{1.0f, 0.0f, 0.0f,	1.0f, 1.0f, 0.0f,	1.0f, 1.0f, 1.0f},
+			{1.0f, 0.0f, 0.0f,	1.0f, 1.0f, 1.0f,	1.0f, 1.0f, 1.0f},
+
+			// NORTH
+			{1.0f, 0.0f, 1.0f,	1.0f, 1.0f, 1.0f,	0.0f, 1.0f, 1.0f},
+			{1.0f, 0.0f, 1.0f,	0.0f, 1.0f, 1.0f,	0.0f, 0.0f, 1.0f},
+
+			// WEST
+			{0.0f, 0.0f, 1.0f,	0.0f, 1.0f, 1.0f,	0.0f, 1.0f, 0.0f},
+			{0.0f, 0.0f, 1.0f,	0.0f, 1.0f, 0.0f,	0.0f, 0.0f, 0.0f},
+
+			// TOP 
+			{0.0f, 1.0f, 0.0f,	0.0f, 1.0f, 1.0f,	1.0f, 1.0f, 1.0f},
+			{0.0f, 1.0f, 0.0f,	1.0f, 1.0f, 1.0f,	1.0f, 1.0f, 0.0f},
+			
+			// BOTTOM
+			{1.0f, 0.0f, 1.0f,	0.0f, 0.0f, 1.0f,	0.0f, 0.0f, 0.0f},
+			{1.0f, 0.0f, 1.0f,	0.0f, 0.0f, 0.0f,	1.0f, 0.0f, 0.0f},
+
+		};
+		
 		return true;
 	}
 
